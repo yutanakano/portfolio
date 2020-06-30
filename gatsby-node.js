f3de9ce3d342fd758c974a0e01d4c405a9bdfcc3
yutanakano <yutanakano395@icloud.com>
2020-07-01 05:11:49 +0900

    記事ページを作成するクエリを追加

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path')
+exports.createPages = async({graphql, actions}) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve('./src/templates/postDetail.js')
+  const result = await graphql(`
+  query {
+    allContentfulPost {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }
+  `)
+
+  result.data.allContentfulPost.edges.forEach((edge) => {
+    createPage({
+      component: blogTemplate,
+      path: `/posts/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug
+      }
+    })
+  })
+}
