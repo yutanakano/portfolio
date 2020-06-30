408699f161c12e286094f6d01c6f6c454b0ebc70
yutanakano <yutanakano395@icloud.com>
2020-07-01 04:39:15 +0900

    gatsby-transformer-remarkの設定を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,20 @@ module.exports = {
         host: process.env.CONTENTFUL_HOST,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    }
   ],
 }
