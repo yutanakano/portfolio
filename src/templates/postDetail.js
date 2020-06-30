84882fe1cf1c8bee917767c432d0e14c02323bd9
yutanakano <yutanakano395@icloud.com>
2020-07-01 05:10:19 +0900

    記事ページのテンプレートを追加

---
+++ src/templates/postDetail.js
@@ -0,0 +1,71 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import { MdFolderOpen, MdLabelOutline, MdUpdate } from 'react-icons/md';
+
+const PostPage =  props => (
+  <Layout>
+    <SEO title="Post" />
+    <div className="section">
+      <div className="container">
+        <img className="image" src={props.data.contentfulPost.image.file.url} />                    
+        <div className="card-content">
+          <time><MdUpdate />{props.data.contentfulPost.createdAt}</time>
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-size-5" to="#">{props.data.contentfulPost.title}</p>
+            </div>
+          </div>
+          <article dangerouslySetInnerHTML={{__html: props.data.contentfulPost.content.childMarkdownRemark.html}}>
+          </article>
+          <div className="column">
+            <div>
+              <p className="tag is-link" to='#'><MdFolderOpen />{props.data.contentfulPost.category.name}</p>
+            </div>
+            <div>
+              {props.data.contentfulPost.tags.map(tags => 
+                <p className="tag is-multiline" to='#'><MdLabelOutline />{tags.name}</p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default PostPage
+
+export const query = graphql`
+  query ($slug: String) {
+    contentfulPost(slug: {eq: $slug}) {
+      id
+      title
+      createdAt(formatString: "YYYY年MM月DD日")
+      image {
+        id
+        file {
+          url
+        }
+      }
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+      category {
+        id
+        name
+        slug
+      }
+      tags {
+        id
+        name
+        slug
+      }
+    }
+  }
+`
