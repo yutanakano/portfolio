8a9f27bd16a3ca3f3c6bb5286a0c881104302220
yutanakano <yutanakano395@icloud.com>
2020-07-05 23:14:58 +0900

    SEO: meta情報登録

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `永久機関`,
+    description: `元ネトゲ廃人の生存戦略ブログ`,
+    author: `@yutanakano`,
   },
   plugins: [
     `gatsby-plugin-sass`,
